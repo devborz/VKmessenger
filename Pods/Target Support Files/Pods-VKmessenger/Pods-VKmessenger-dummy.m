eb82fcea1e18bbf4e6524a2b04a6823bd59b185b
Usman Turkaev <u.turkaev@yandex.ru>
2020-09-30 19:38:18 +0300

    Intsalled VK-sdk

---
+++ Pods/Target Support Files/Pods-VKmessenger/Pods-VKmessenger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VKmessenger : NSObject
+@end
+@implementation PodsDummy_Pods_VKmessenger
+@end
---
+++ VKmessenger/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  VKmessenger
+//
+//  Created by Усман Туркаев on 30.09.2020.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
