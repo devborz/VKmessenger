e452b0800c9d24d66a3b25934cbbfe375b07c656
Usman Turkaev <u.turkaev@yandex.ru>
2020-09-30 18:11:23 +0300

    Initial commit

---
+++ VKmessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKmessenger
+//
+//  Created by Усман Туркаев on 30.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
